726e0348c6fddc5d9cd04bc83bae217f0afabaa7
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-04 14:40:56 -0300

    Home Screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+
 @IonicPage()
-
 @Component({
-
   selector: 'page-home',
   templateUrl: 'home.html'
-
 })
 
 export class HomePage {
