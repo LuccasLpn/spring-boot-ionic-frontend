a0551c922360eb415b09f79297165fbb44ac0e54
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-05 13:19:56 -0300

    Saving data to localStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,13 +2,16 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { CredenciaisDTO } from "../models/credenciais.dto";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storage.services";
 
 @Injectable()
 export class AuthService{
     basepath = "/login"
 
     constructor(public http: HttpClient,
-                private _platform: Platform){
+                private _platform: Platform,
+                public storage : StorageService){
 
                  if(this._platform.is("cordova")){
                      this.basepath = "http://localhost:8080";
@@ -23,5 +26,15 @@ export class AuthService{
             observe : 'response',
             responseType: 'text'
         });
+    }
+    successfulllogin(authorizationValue : string){
+        let tok = authorizationValue.substring(7);
+        let user : LocalUser = {
+            token : tok
+        };
+        this.storage.setLocalUser(user);
+    }
+    logout(){
+        this.storage.setLocalUser(null);
     }
 }
